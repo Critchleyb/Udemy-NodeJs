bc927554e066af13d7872462a4c103962e41e40c
Critchleyb <Critchley_11@hotmail.co.uk>
2020-10-06 17:24:54 +0100

    End of lecture 67

---
+++ 4-natours/starter/controllers/tourController.js
@@ -0,0 +1,90 @@
+const fs = require('fs');
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+
+exports.checkID = (req, res, next, val) => {
+    const tourIndex = tours.findIndex((el) => {
+        return el.id === +val;
+    });
+    if (tourIndex === -1) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID',
+        });
+    } else {
+        req.tourIndex = tourIndex;
+        next();
+    }
+};
+
+exports.checkBody = (req, res, next) => {
+    if (req.body.name && req.body.price) {
+        next();
+    } else {
+        return res.status(400).json({
+            status: 'fail',
+            message: 'Body is missing name or price key',
+        });
+    }
+};
+
+exports.getAllTours = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        requestTime: req.requestTime,
+        results: tours.length,
+        data: {
+            tours,
+        },
+    });
+};
+
+exports.getTour = (req, res) => {
+    return res.status(200).json({
+        status: 'success',
+        data: {
+            tour: tours[req.tourIndex],
+        },
+    });
+};
+
+exports.createTour = (req, res) => {
+    //   console.log(req.body);
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        });
+    });
+};
+
+exports.patchTour = (req, res) => {
+    const updatedTour = {
+        ...tours[req.tourIndex],
+        ...req.body,
+    };
+    tours[req.tourIndex] = updatedTour;
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                updatedTour,
+            },
+        });
+    });
+};
+
+exports.deleteTour = (req, res) => {
+    //TODO  DELETE LOGIC WOULD GO HERE
+    res.status(204).json({
+        status: 'success',
+        data: null,
+    });
+};
---
+++ 4-natours/starter/controllers/userController.js
@@ -0,0 +1,44 @@
+exports.getAllUsers = (req, res) => {
+    //TODO
+
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined',
+    });
+};
+
+exports.getUser = (req, res) => {
+    //TODO
+
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined',
+    });
+};
+
+exports.createUser = (req, res) => {
+    //TODO
+
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined',
+    });
+};
+
+exports.updateUser = (req, res) => {
+    //TODO
+
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined',
+    });
+};
+
+exports.deleteUser = (req, res) => {
+    //TODO
+
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined',
+    });
+};
