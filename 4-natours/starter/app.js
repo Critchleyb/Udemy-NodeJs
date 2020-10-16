4fce141520b32107ea5a589204b473c1955eee00
Critchleyb <Critchley_11@hotmail.co.uk>
2020-10-16 16:41:15 +0100

    End of lecture 115

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes');
@@ -14,12 +16,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use((req, res, next) => {
-    // console.log('Hello from the middleware');
-    // MIDDLEWARE GOES HERE
-    next();
-});
-
-app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
@@ -29,5 +25,11 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+    next(new AppError(`URL: ${req.originalUrl} not found`, 404)); //If a param is used inside next(), it will always be considered an error.
+});
+
+app.use(globalErrorHandler); //If the function passed in to app.use has 4 params it is auto used as the global error handler middleware.
 
 module.exports = app;
